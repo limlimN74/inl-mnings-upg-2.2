c5e1c9a7c7d944e1808b98f2b837094cdbc0fd69
lini07003 <lini07003@E-PW05C37N>
2024-10-09 12:10:41 +0200

    Add project files.

---
+++ inlämnings upg 2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+namespace inlämnings_upg_2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Frågar elin//
+            Console.WriteLine("Hur långt hoppade Elin? ");
+            string lägndtext1= Console.ReadLine();
+            double lägnd1= double.Parse(lägndtext1 );
+
+            //Frågar Alma//
+            Console.WriteLine("Hur lågnt hoppade Alma? ");
+            string lägndtext2= Console.ReadLine();
+            double lägnd2=double.Parse(lägndtext2 );
+
+            //Skillnad//
+            double skillnad = lägnd1 - lägnd2;
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre");
+        }
+    }
+}
